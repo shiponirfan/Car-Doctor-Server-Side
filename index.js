c001bef57b8e3659f1dab283540c3bce267f8288
shiponirfan <imshiponirfan@gmail.com>
2023-10-29 01:07:43 +0600

    Cart Details API Added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Middleware
@@ -26,9 +26,26 @@ async function run() {
     await client.connect();
 
     const servicesCollection = client.db("carDoctorDB").collection("services");
+    const productsCollection = client.db("carDoctorDB").collection("products");
+    const cartsCollection = client.db("carDoctorDB").collection("cartDetails");
 
     app.get("/services", async (req, res) => {
       const result = await servicesCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/products", async (req, res) => {
+      const result = await productsCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await servicesCollection.findOne(query);
+      res.send(result);
+    });
+    app.post("/cartDetails", async (req, res) => {
+      const carts = req.body;
+      const result = await cartsCollection.insertOne(carts);
       res.send(result);
     });
 
