6aed13b688b263c5be1ed9c614c6cf0c0f02f6ee
shiponirfan <imshiponirfan@gmail.com>
2023-10-31 22:30:47 +0600

    JWT Token System Added

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5174", "http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-ujyuzy1-shard-00-00.pzomx9u.mongodb.net:27017,ac-ujyuzy1-shard-00-01.pzomx9u.mongodb.net:27017,ac-ujyuzy1-shard-00-02.pzomx9u.mongodb.net:27017/?ssl=true&replicaSet=atlas-lf5h1l-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
@@ -20,6 +28,25 @@ const client = new MongoClient(uri, {
   },
 });
 
+// Custom Middleware
+const logger = (req, res, next) => {
+  console.log("Log Info: ", req.method, req.url);
+  next();
+};
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -29,6 +56,28 @@ async function run() {
     const productsCollection = client.db("carDoctorDB").collection("products");
     const cartsCollection = client.db("carDoctorDB").collection("cartDetails");
 
+    // Jwt Related API
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_KEY, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .send({ success: true });
+    });
+
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      res.clearCookie("token", { maxAge: 0 }).send({ success: true });
+    });
+
+    // Services Related API
     app.get("/services", async (req, res) => {
       const result = await servicesCollection.find().toArray();
       res.send(result);
@@ -43,7 +92,10 @@ async function run() {
       const result = await servicesCollection.findOne(query);
       res.send(result);
     });
-    app.get("/cartDetails", async (req, res) => {
+    app.get("/cartDetails", logger, verifyToken, async (req, res) => {
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: "Forbidden" });
+      }
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
